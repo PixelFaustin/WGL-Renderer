feed5319b45c3beea1802014a10095c664d4f06c
12xp12 <mckenna.au@husky.neu.edu>
2018-03-27 19:18:04 -0400

    Project init

---
+++ src/TextureManager.js
@@ -0,0 +1,34 @@
+import Device from './Device';
+
+export default class TextureManager {
+  constructor(gl) {
+    this.gl = gl;
+    this.textureMap = {};
+  }
+
+  downloadTextures = textureMap => {
+    return new Promise.all(
+      Object.keys(textureMap).map(name => {
+        const url = textureMap[name];
+
+        return new Promise((resolve, reject) => {
+          const image = new Image();
+          image.onload = () => {
+            resolve({ name, image });
+          };
+          image.onerror = () => {
+            reject(`Could not load image at url ${url}`);
+          };
+          image.src = url;
+        });
+      })
+    ).then(downloadedItems => {
+      downloadedItems.forEach(({ name, image }) => {
+        this.textureMap[name] = Device.createTexture2D(
+          image.width,
+          image.height
+        ).copyFromImage(image);
+      });
+    });
+  };
+}
